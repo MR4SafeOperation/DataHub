e8366dab39915f3f63d228f1044194bb7b09c198
Selchow <jan.selchow@x-visual.com>
2025-02-24 14:14:12 +0100

    added XVisual resovler and restVorward

--- mr4so_module/src/endpoints.js
+++ mr4so_module/src/endpoints.js
@@ -1,4 +1,5 @@
-﻿export function applyEndpoints({app}) {
+﻿import axios from 'axios';
+export function applyEndpoints({app}) {
     app.get('/hello_world', (req, res) => {
         res.send('Hello World');
     });
@@ -6,6 +7,46 @@
     app.get('/hello_user', (req, res) => {
         res.send('Hello, ' + res.locals?.oauth?.token?.user?.username);
     });
-
-
+    const xVisualEndpoint = 'http://xvdatahub.azurewebsites.net';
+    app.get('/xvforward',async  (req, res) => {
+        try {
+            console.log("Querry",res.req.query)
+            let relUrl =  res.req.query.path;
+            const fullUrl = new URL(relUrl, xVisualEndpoint);
+       
+            console.log("forwading target:",fullUrl.href)
+            // Make a request to the other REST endpoint
+            const response = await axios.get(fullUrl.href, {
+              responseType: 'stream' ,// Set responseType to stream to handle all types of data
+              headers: {
+                'X-Api-Key': '12D705AEDAFA49B5AAF11EF79766845D'
+              }
+            });
+        
+            // Get the content type from the response headers
+            const contentType = response.headers['content-type'];
+        
+            // Set the appropriate headers for the response
+            res.setHeader('Content-Type', contentType);
+        
+            // Handle different content types
+            if (contentType.includes('application/json')) {
+              let data = '';
+              response.data.on('data', (chunk) => {
+                data += chunk;
+              });
+              response.data.on('end', () => {
+                res.json(JSON.parse(data));
+              });
+            } else {
+              // For other types of data, pipe the response directly
+              response.data.pipe(res);
+            }
+          } catch (error) {
+            console.error(error);
+            // Handle any errors
+            res.status(500).send('Error forwarding the response');
+          }
+        });
 }
+
--- mr4so_module/src/resolvers.js
+++ mr4so_module/src/resolvers.js
@@ -49,22 +49,78 @@ async function roesbergData(parent, args, context, info) {
 async function roesbergDataAggregate(parent, args, context, info) {
     const res = await fetch(FAKE_API_URL);
     const resJson = await res.json();
-    return {count: resJson.total};
+    return { count: resJson.total };
 }
+
+///XVisual Resolver 
+const xVisualEndpoint = 'https://xvdatahub.azurewebsites.net/api/';
+async function readStream(body) {
+    const reader = body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    let result = '';
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break; result += decoder.decode(value, { stream: true });
+    } result += decoder.decode(); // Flush the decoder return result; 
+    return result;
+}
+async function ResolveXVisualApi(parent, args, context, info) {
+    // Check https://reqres.in/ to see the data structure of the fake API.
+    console.log("parent :", parent);
+    console.log("args :", args);
+    console.log("info :", info);
+    console.log("context type", typeof context);
+    console.log("context :", context.req.body);
+
+    try {
+        console.log("Body", context.req.body);
+        var rawbody = context.req.body;
+        // Parse the JSON string into an object 
+        // const jsonObjectBody = JSON.parse(rawbody);
+        // // Access the query property c
+        // const query = jsonObject.query;
+        console.log("ExtractedQuerry", rawbody.query);
+
+        const response = await fetch(xVisualEndpoint, {
+            method: 'POST', headers: {
+                'Content-Type': 'application/json',
+                'X-Api-Key': '12D705AEDAFA49B5AAF11EF79766845D'
+            }, body: JSON.stringify(context.req.body)
+        });
+
+        var bodyResult = await readStream(response.body);
+        // The JSON string
+        console.log("bodyResult", bodyResult);
+        const jsonObject = JSON.parse(bodyResult); // Access the inner list (projects array)
+        const projectsList = jsonObject.data.projects;
+        console.log("Response body", bodyResult);
+        return jsonObject.data[info.fieldName];
+    } catch (error) {
+        // Handle any errors that occur
+        console.error('An error occurred:', error);
+    }
+
+}
+
+
+
+
+
 
 
 const resolvers = {
     Query: {
         // The following two overwrite the auto-generated resolvers for the `ExternalUser` type
-		roesbergData,
-		roesbergDataAggregate
+        roesbergData,
+        roesbergDataAggregate,
+
+        //xvisualSchema
+        projects: ResolveXVisualApi,
+        drawing: ResolveXVisualApi,
+        drawingSVG: ResolveXVisualApi,
     },
 }
 
 export const getResolvers = () => resolvers;
-
-
-
-
 
 
